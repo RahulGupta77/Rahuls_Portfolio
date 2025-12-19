9d4f90a8606a37cc192f1f399f459ee2b866a1e5
rahul-gupta-atlas <rahul.gupta@helloatlas.in>
2025-12-19 11:16:22 +0530

    updated message

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "Software Engineer",
   description:
-    "A passionate Full-Stack developer with expertise in React, Flask, and cloud platforms like GCP and AWS. Skilled in Kubernetes and Docker, I specialize in scalable web applications. Currently exploring GenAI and Agentic Workflows. Always eager for innovative projects—and up for a snooker game! 😊",
+    "A passionate Full-Stack developer with expertise in React, Flask, and cloud platforms like GCP and AWS. Skilled in Kubernetes and Docker, I specialize in scalable web applications. Currently exploring GenAI and Agentic Workflows. Always eager for innovative projects and up for a snooker game! 😊",
   email: "rahulgupta34981@gmail.com",
   phone: "+918766030074",
   address: "Mumbai, Maharashtra",
