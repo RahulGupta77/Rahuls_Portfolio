472a451c3fd97172ecaee2f453d0879a30fe76dd
Rahul Gupta <rg3498104@gmail.com>
2026-07-23 04:50:53 +0530

    update personal credentials

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,14 +4,12 @@ export const personalData = {
   designation: "Software Engineer",
   description:
     "A passionate Full-Stack developer with expertise in React, Flask, and cloud platforms like GCP and AWS. Skilled in Kubernetes and Docker, I specialize in scalable web applications. Currently exploring GenAI and Agentic Workflows. Always eager for innovative projects and up for a snooker game! 😊",
-  email: "rahulgupta34981@gmail.com",
-  phone: "+918766030074",
   address: "Mumbai, Maharashtra",
   github: "https://github.com/RahulGupta77",
   linkedIn: "https://www.linkedin.com/in/rahul-gupta-aa60611a7/",
-  twitter: "https://x.com/rahulguptaa29",
+  twitter: "https://x.com/rahulspace_",
   leetcode: "https://leetcode.com/u/RahulGupta77/",
-  devUsername: "rahulgupta77",
-  resume:
-    "https://drive.google.com/file/d/1ygQQUVtxe75mdFJh0siW1cZ97VF0Mm0k/view?usp=sharing",
+  devUsername: "rahulspace",
+  // resume:
+  //   "https://drive.google.com/file/d/1ygQQUVtxe75mdFJh0siW1cZ97VF0Mm0k/view?usp=sharing",
 };
